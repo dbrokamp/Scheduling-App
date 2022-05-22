d97d45f1d22a5cbec7814597e1eb6c638ed1f5b2
Drew Brokamp <drew.brokamp@gmail.comm>
2022-05-22 17:46:12 -0400

    Fixed return errors in verifyUsername and verifyPassword

--- src/main/java/com/company/schedulingapp/dbaccess/DBUsers.java
+++ src/main/java/com/company/schedulingapp/dbaccess/DBUsers.java
@@ -10,20 +10,20 @@ import java.sql.SQLException;
 public class DBUsers {
 
     public static boolean verifyUsername(String username) throws SQLException {
+
+
         Connection connection = JDBC.getConnection();
         String sql = "SELECT * FROM users WHERE User_Name = ?";
         PreparedStatement userWithUsername = connection.prepareStatement(sql);
         userWithUsername.setString(1, username);
         ResultSet user = userWithUsername.executeQuery();
         if (user.next()) {
-            if (user.getString("User_Name") == username) {
-                return true;
-            } else {
-                return false;
-            }
+            return true;
         } else {
             return false;
         }
+
+
     }
     public static boolean verifyPassword(String password) throws SQLException {
         Connection connection = JDBC.getConnection();
@@ -32,11 +32,7 @@ public class DBUsers {
         userWithUsername.setString(1, password);
         ResultSet user = userWithUsername.executeQuery();
         if (user.next()) {
-            if (user.getString("Password") == password) {
-                return true;
-            } else {
-                return false;
-            }
+            return true;
         } else {
             return false;
         }
