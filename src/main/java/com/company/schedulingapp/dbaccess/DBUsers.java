4ee712199d975f4b1ad4d12fe51c05c02243458d
Drew Brokamp <drew.brokamp@gmail.comm>
2022-05-22 17:27:04 -0400

    Added verifyUsername and verifyPassword

--- src/main/java/com/company/schedulingapp/dbaccess/DBUsers.java
+++ src/main/java/com/company/schedulingapp/dbaccess/DBUsers.java
@@ -1,4 +1,46 @@
 package com.company.schedulingapp.dbaccess;
 
+import com.company.schedulingapp.util.JDBC;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DBUsers {
+
+    public static boolean verifyUsername(String username) throws SQLException {
+        Connection connection = JDBC.getConnection();
+        String sql = "SELECT * FROM users WHERE User_Name = ?";
+        PreparedStatement userWithUsername = connection.prepareStatement(sql);
+        userWithUsername.setString(1, username);
+        ResultSet user = userWithUsername.executeQuery();
+        if (user.next()) {
+            if (user.getString("User_Name") == username) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    public static boolean verifyPassword(String password) throws SQLException {
+        Connection connection = JDBC.getConnection();
+        String sql = "SELECT * FROM users WHERE Password = ?";
+        PreparedStatement userWithUsername = connection.prepareStatement(sql);
+        userWithUsername.setString(1, password);
+        ResultSet user = userWithUsername.executeQuery();
+        if (user.next()) {
+            if (user.getString("Password") == password) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+
 }
