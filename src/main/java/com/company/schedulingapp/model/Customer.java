0e49b3791d1d9bc529ac0e7da0da34abb3650b4a
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-02 11:25:21 -0400

    Removed created, and updated fields

--- src/main/java/com/company/schedulingapp/model/Customer.java
+++ src/main/java/com/company/schedulingapp/model/Customer.java
@@ -10,23 +10,16 @@ public class Customer {
     private String address;
     private String postalCode;
     private String phone;
-    private Date createDate;
-    private String createdBy;
-    private Timestamp lastUpdate;
-    private String lastUpdatedBy;
     private Integer divisionID;
 
 
-    public Customer(Integer customerID, String customerName, String address, String postalCode, String phone, Date createDate, String createdBy, Timestamp lastUpdate, String lastUpdatedBy, Integer divisionID) {
+    public Customer(Integer customerID, String customerName, String address, String postalCode, String phone, Integer divisionID) {
         this.customerID = customerID;
         this.customerName = customerName;
         this.address = address;
         this.postalCode = postalCode;
         this.phone = phone;
-        this.createDate = createDate;
-        this.createdBy = createdBy;
-        this.lastUpdate = lastUpdate;
-        this.lastUpdatedBy = lastUpdatedBy;
+
         this.divisionID = divisionID;
     }
 
@@ -68,38 +61,6 @@ public class Customer {
 
     public void setPhone(String phone) {
         this.phone = phone;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Timestamp getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Timestamp lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-
-    public String getLastUpdatedBy() {
-        return lastUpdatedBy;
-    }
-
-    public void setLastUpdatedBy(String lastUpdatedBy) {
-        this.lastUpdatedBy = lastUpdatedBy;
     }
 
     public Integer getDivisionID() {
