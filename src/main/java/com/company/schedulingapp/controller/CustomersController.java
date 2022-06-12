d949bf21fe2fd97857b991bddf7241330cc36d89
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-12 16:09:39 -0400

    Removed debug statement

--- src/main/java/com/company/schedulingapp/controller/CustomersController.java
+++ src/main/java/com/company/schedulingapp/controller/CustomersController.java
@@ -1,14 +1,18 @@
 package com.company.schedulingapp.controller;
 
+import com.company.schedulingapp.dbaccess.DBAppointments;
 import com.company.schedulingapp.dbaccess.DBCustomers;
+import com.company.schedulingapp.model.Appointment;
 import com.company.schedulingapp.model.Customer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
@@ -22,19 +26,47 @@ public class CustomersController implements Initializable {
     @FXML TableColumn<Customer, String> customerPhoneColumn = new TableColumn<>("Phone");
     @FXML TableColumn<Customer, Integer> customerDivisionIDColumn = new TableColumn<>("Division ID");
 
+    @FXML TableView<Appointment> appointmentTableView = new TableView<>();
+    @FXML TableColumn<Appointment, Integer> appointmentIDColumn = new TableColumn<>("Appointment_ID");
+    @FXML TableColumn<Appointment, String> appointmentTitleColumn = new TableColumn<>("Title");
+    @FXML TableColumn<Appointment, String> appointmentDescriptionColumn = new TableColumn<>("Description");
+    @FXML TableColumn<Appointment, String> appointmentLocationColumn = new TableColumn<>("Location");
+    @FXML TableColumn<Appointment, String> appointmentTypeColumn = new TableColumn<>("Type");
+    @FXML TableColumn<Appointment, Date> appointmentStartColumn = new TableColumn<>("Start");
+    @FXML TableColumn<Appointment, Date> appointmentEndColumn = new TableColumn<>("End");
+    @FXML TableColumn<Appointment, Integer> appointmentCustomerIDColumn = new TableColumn<>("Customer_ID");
+    @FXML TableColumn<Appointment, Integer> appointmentUserIDColumn = new TableColumn<>("User_ID");
+    @FXML TableColumn<Appointment, Integer> appointmentContactIDColumn = new TableColumn<>("Contact_ID");
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setupCustomerTable();
+        setupAppointmentTable();
 
         customerTableView.getSelectionModel().selectedItemProperty().addListener((observable, oldSelection, newSelection) -> {
             if (newSelection != null) {
-                System.out.println(newSelection.getCustomerName());
+                try {
+                    appointmentTableView.setItems(DBAppointments.getCustomerAppointments(newSelection.getCustomerID()));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
+    }
 
-
+    private void setupAppointmentTable() {
+        appointmentIDColumn.setCellValueFactory(new PropertyValueFactory<>("appointmentID"));
+        appointmentTitleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        appointmentDescriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        appointmentLocationColumn.setCellValueFactory(new PropertyValueFactory<>("location"));
+        appointmentTypeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        appointmentStartColumn.setCellValueFactory(new PropertyValueFactory<>("start"));
+        appointmentEndColumn.setCellValueFactory(new PropertyValueFactory<>("end"));
+        appointmentCustomerIDColumn.setCellValueFactory(new PropertyValueFactory<>("customerID"));
+        appointmentUserIDColumn.setCellValueFactory(new PropertyValueFactory<>("userID"));
+        appointmentContactIDColumn.setCellValueFactory(new PropertyValueFactory<>("contactID"));
     }
 
     private void setupCustomerTable() {
