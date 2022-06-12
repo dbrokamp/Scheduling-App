cfb68069fc3a57822d499de39eb5caf118a9b928
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-12 16:56:07 -0400

    Added FXML variables for fields

--- src/main/java/com/company/schedulingapp/controller/AddCustomerController.java
+++ src/main/java/com/company/schedulingapp/controller/AddCustomerController.java
@@ -3,7 +3,8 @@ package com.company.schedulingapp.controller;
 import com.company.schedulingapp.util.SceneController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 
 public class AddCustomerController {
@@ -16,6 +17,12 @@ public class AddCustomerController {
     private String newCustomerPhone;
     private String newCustomerState;
     private String newCustomerCountry;
+
+    @FXML TextField name;
+    @FXML TextField address;
+    @FXML TextField postalCode;
+    @FXML ComboBox<String> country;
+    @FXML ComboBox<String> state;
 
 
     public void save() {
