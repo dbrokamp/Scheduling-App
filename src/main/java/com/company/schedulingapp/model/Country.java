f7f1da9a157c2f12b34c24dc14895c205d0574e7
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-13 16:18:48 -0400

    Removed unneeded timestamp variables

--- src/main/java/com/company/schedulingapp/model/Country.java
+++ src/main/java/com/company/schedulingapp/model/Country.java
@@ -7,18 +7,12 @@ public class Country {
 
     private Integer countryID;
     private String country;
-    private Date createDate;
-    private String createdBy;
-    private Timestamp lastUpdate;
-    private String lastUpdatedBy;
 
-    public Country(Integer countryID, String country, Date createDate, String createdBy, Timestamp lastUpdate, String lastUpdatedBy) {
+
+    public Country(Integer countryID, String country) {
         this.countryID = countryID;
         this.country = country;
-        this.createDate = createDate;
-        this.createdBy = createdBy;
-        this.lastUpdate = lastUpdate;
-        this.lastUpdatedBy = lastUpdatedBy;
+
     }
 
     public Integer getCountryID() {
@@ -37,35 +31,4 @@ public class Country {
         this.country = country;
     }
 
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Timestamp getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Timestamp lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-
-    public String getLastUpdatedBy() {
-        return lastUpdatedBy;
-    }
-
-    public void setLastUpdatedBy(String lastUpdatedBy) {
-        this.lastUpdatedBy = lastUpdatedBy;
-    }
 }
