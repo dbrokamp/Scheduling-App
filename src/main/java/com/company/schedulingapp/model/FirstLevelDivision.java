8def0888029f66cefcd223e7cb323ad9495b06fa
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-13 16:18:43 -0400

    Removed unneeded timestamp variables

--- src/main/java/com/company/schedulingapp/model/FirstLevelDivision.java
+++ src/main/java/com/company/schedulingapp/model/FirstLevelDivision.java
@@ -7,19 +7,12 @@ public class FirstLevelDivision {
 
     private Integer divisionID;
     private String division;
-    private Date createdDate;
-    private String createdBy;
-    private Timestamp lastUpdate;
-    private String lastUpdatedBy;
     private Integer countryID;
 
-    public FirstLevelDivision(Integer divisionID, String division, Date createdDate, String createdBy, Timestamp lastUpdate, String lastUpdatedBy, Integer countryID) {
+    public FirstLevelDivision(Integer divisionID, String division, Integer countryID) {
         this.divisionID = divisionID;
         this.division = division;
-        this.createdDate = createdDate;
-        this.createdBy = createdBy;
-        this.lastUpdate = lastUpdate;
-        this.lastUpdatedBy = lastUpdatedBy;
+
         this.countryID = countryID;
     }
 
@@ -37,38 +30,6 @@ public class FirstLevelDivision {
 
     public void setDivision(String division) {
         this.division = division;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Timestamp getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Timestamp lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-
-    public String getLastUpdatedBy() {
-        return lastUpdatedBy;
-    }
-
-    public void setLastUpdatedBy(String lastUpdatedBy) {
-        this.lastUpdatedBy = lastUpdatedBy;
     }
 
     public Integer getCountryID() {
