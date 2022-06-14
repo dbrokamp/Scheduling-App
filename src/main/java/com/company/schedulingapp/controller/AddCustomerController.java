9fec2fd8e7c8366726eae55041d8c366e91f7d1c
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-14 14:46:22 -0400

    Added methods to populate first level division combo box based on country selection

--- src/main/java/com/company/schedulingapp/controller/AddCustomerController.java
+++ src/main/java/com/company/schedulingapp/controller/AddCustomerController.java
@@ -1,13 +1,24 @@
 package com.company.schedulingapp.controller;
 
+import com.company.schedulingapp.dbaccess.DBCountries;
+import com.company.schedulingapp.dbaccess.DBFirstLevelDivisions;
+import com.company.schedulingapp.model.Country;
+import com.company.schedulingapp.model.FirstLevelDivision;
 import com.company.schedulingapp.util.SceneController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class AddCustomerController {
+
+public class AddCustomerController implements Initializable {
 
     SceneController sceneController = SceneController.getSceneControllerInstance();
 
@@ -18,20 +29,68 @@ public class AddCustomerController {
     private String newCustomerState;
     private String newCustomerCountry;
 
-    @FXML TextField name;
-    @FXML TextField address;
-    @FXML TextField postalCode;
-    @FXML ComboBox<String> country;
-    @FXML ComboBox<String> state;
+    @FXML TextField nameTextField;
+    @FXML TextField addressTextField;
+    @FXML TextField postalCodeTextField;
+    @FXML ComboBox<String> countryComboBox;
+    @FXML ComboBox<String> firstLevelDivisionComboBox;
 
+    ObservableList<String> countryNames = FXCollections.observableArrayList();
+    ObservableList<String> firstLevelDivisionNames = FXCollections.observableArrayList();
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        getCountryNames();
+        setCountryComboBox();
+
+
+        countryComboBox.getSelectionModel().selectedItemProperty().addListener((options, oldValue, newValue) -> {
+            try {
+                getFirstLevelDivisionNamesForSelectedCountry(newValue);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            setFirstLevelDivisionComboBox();
+
+        });;
+
+    }
+
+    private void getCountryNames() {
+        try {
+            for (Country country : DBCountries.getCountries()){
+                countryNames.add(country.getCountry());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setCountryComboBox() {
+        countryComboBox.setItems(countryNames);
+    }
+
+    private void getFirstLevelDivisionNamesForSelectedCountry(String countryName) throws SQLException {
+        Country country = DBCountries.getCountryByName(countryName);
+
+        for (FirstLevelDivision firstLevelDivision : DBFirstLevelDivisions.getFirstLevelDivisionsForCountryID(country.getCountryID())) {
+            firstLevelDivisionNames.add(firstLevelDivision.getDivision());
+        }
+    }
+
+    private void setFirstLevelDivisionComboBox() {
+        firstLevelDivisionComboBox.setItems(firstLevelDivisionNames);
+    }
 
     public void save() {
 
     }
 
     public void cancel(ActionEvent event) {
-        sceneController.setScene(event, "Customers");
+        sceneController.setScene(event, "Customers.fxml");
 
     }
+
 
 }
