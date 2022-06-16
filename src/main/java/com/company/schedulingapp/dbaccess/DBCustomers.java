cddc3e6b28776e3551913775e428aa48c02ec859
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-16 14:32:11 -0400

    Corrected addNewCustomer method

--- src/main/java/com/company/schedulingapp/dbaccess/DBCustomers.java
+++ src/main/java/com/company/schedulingapp/dbaccess/DBCustomers.java
@@ -5,10 +5,9 @@ import com.company.schedulingapp.util.JDBC;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.sql.*;
+import java.time.LocalDateTime;
 
 public class DBCustomers {
 
@@ -41,7 +40,7 @@ public class DBCustomers {
         return customers;
     }
 
-    public static Integer createNewCustomerID() throws SQLException {
+    private static Integer createNewCustomerID() throws SQLException {
         if (customers.isEmpty()) {
             getCustomers();
         }
@@ -49,5 +48,22 @@ public class DBCustomers {
         Customer currentLastCustomer = customers.get(customers.size() - 1);
 
         return currentLastCustomer.getCustomerID() + 1;
+    }
+
+    public static void addNewCustomer(String newCustomerName, String newCustomerAddress, String newCustomerPostalCode, String newCustomerPhone, String firstLevelDivisionName) throws SQLException {
+        Connection connection = JDBC.getConnection();
+        String sql = "INSERT INTO Customers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement newCustomerStatement = connection.prepareStatement(sql);
+        newCustomerStatement.setInt(1, createNewCustomerID());
+        newCustomerStatement.setString(2, newCustomerName);
+        newCustomerStatement.setString(3, newCustomerAddress);
+        newCustomerStatement.setString(4, newCustomerPostalCode);
+        newCustomerStatement.setString(5, newCustomerPhone);
+        newCustomerStatement.setTimestamp(6, Timestamp.valueOf(LocalDateTime.now())); // Create Date
+        newCustomerStatement.setString(7, DBUsers.getCurrentUserName()); // Created_By
+        newCustomerStatement.setTimestamp(8, Timestamp.valueOf(LocalDateTime.now())); // Last updated
+        newCustomerStatement.setString(9, DBUsers.getCurrentUserName()); // Last updated by
+        newCustomerStatement.setInt(10, DBFirstLevelDivisions.getDivisionID(firstLevelDivisionName));
+        newCustomerStatement.executeUpdate();
     }
 }
