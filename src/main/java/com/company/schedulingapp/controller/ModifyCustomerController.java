ebf6f8dbab03c64f632ac7bca5934dd1adb1d5ce
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-17 11:47:43 -0400

    Save button automatically returns to main

--- src/main/java/com/company/schedulingapp/controller/ModifyCustomerController.java
+++ src/main/java/com/company/schedulingapp/controller/ModifyCustomerController.java
@@ -250,6 +250,8 @@ public class ModifyCustomerController implements Initializable {
         checkPhoneFieldForChanges();
         checkFirstLevelDivisionFieldForChanges();
 
+        returnToMainScene(event);
+
     }
 
 
