01cb4a2234d179d6af3c1cd3fa08ce8eabf7deea
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-17 10:18:21 -0400

    Added methods to get info and populate fields for the selected customer

--- src/main/java/com/company/schedulingapp/controller/ModifyCustomerController.java
+++ src/main/java/com/company/schedulingapp/controller/ModifyCustomerController.java
@@ -1,5 +1,10 @@
 package com.company.schedulingapp.controller;
 
+import com.company.schedulingapp.dbaccess.DBCountries;
+import com.company.schedulingapp.dbaccess.DBFirstLevelDivisions;
+import com.company.schedulingapp.model.Country;
+import com.company.schedulingapp.model.Customer;
+import com.company.schedulingapp.model.FirstLevelDivision;
 import com.company.schedulingapp.util.SceneController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,11 +14,15 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class ModifyCustomerController implements Initializable {
 
     SceneController sceneController = SceneController.getSceneControllerInstance();
+    private Customer customerToModify;
+    private FirstLevelDivision customerToModifyFirstLevelDivision;
+    private Country customerToModifyCountry;
 
     private String newCustomerName;
     private String newCustomerAddress;
@@ -21,12 +30,10 @@ public class ModifyCustomerController implements Initializable {
     private String newCustomerPhone;
     private String newCustomerFirstLevelDivision;
 
-    @FXML
-    TextField nameTextField;
+    @FXML TextField nameTextField;
     @FXML TextField addressTextField;
     @FXML TextField postalCodeTextField;
-    @FXML
-    ComboBox<String> countryComboBox;
+    @FXML ComboBox<String> countryComboBox;
     @FXML ComboBox<String> firstLevelDivisionComboBox;
     @FXML TextField phoneTextField;
 
@@ -34,6 +41,76 @@ public class ModifyCustomerController implements Initializable {
     ObservableList<String> firstLevelDivisionNames = FXCollections.observableArrayList();
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        customerToModify = MainController.getSelectedCustomer();
+        getFirstLevelDivisionForCustomer();
+        getCountryForCustomer();
+
+        getCountryNames();
+        populateCountryComboBox();
+
+
+        getFirstLevelDivisionsForSelectedCountry();
+        populateFirstLevelDivisionComboBox();
+
+        populateFormFieldsWithCustomerData();
 
     }
+
+    private void getCountryNames() {
+        try {
+            for (Country country : DBCountries.getCountries()){
+                countryNames.add(country.getCountry());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void populateCountryComboBox() {
+        countryComboBox.setItems(countryNames);
+    }
+
+    private void populateFormFieldsWithCustomerData() {
+        nameTextField.setText(customerToModify.getCustomerName());
+        addressTextField.setText(customerToModify.getAddress());
+        postalCodeTextField.setText(customerToModify.getPostalCode());
+        countryComboBox.getSelectionModel().select(customerToModifyCountry.getCountry());
+        firstLevelDivisionComboBox.getSelectionModel().select(customerToModifyFirstLevelDivision.getDivision());
+        phoneTextField.setText(customerToModify.getPhone());
+
+
+    }
+
+    private void getFirstLevelDivisionsForSelectedCountry() {
+        try {
+            for (FirstLevelDivision firstLevelDivision : DBFirstLevelDivisions.getFirstLevelDivisionsForCountryID(customerToModifyCountry.getCountryID())) {
+                firstLevelDivisionNames.add(firstLevelDivision.getDivision());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void populateFirstLevelDivisionComboBox() {
+        firstLevelDivisionComboBox.setItems(firstLevelDivisionNames);
+    }
+
+    private void getFirstLevelDivisionForCustomer() {
+        try {
+            customerToModifyFirstLevelDivision = DBFirstLevelDivisions.getFirstLevelDivisionFromDivisionID(customerToModify.getDivisionID());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getCountryForCustomer() {
+        try {
+            customerToModifyCountry = DBCountries.getCountryByID(customerToModifyFirstLevelDivision.getCountryID());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
