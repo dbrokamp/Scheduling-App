c49dec63ef125c940fd0ba648887e38fe1b760c5
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-17 20:07:18 -0400

    Removed unneeded instance variables, outside of scope of application

--- src/main/java/com/company/schedulingapp/model/User.java
+++ src/main/java/com/company/schedulingapp/model/User.java
@@ -7,20 +7,13 @@ public class User {
 
     private Integer userID;
     private String userName;
-    private String password;
-    private Date createDate;
-    private String createdBy;
-    private Timestamp lastUpdate;
-    private String lastUpdatedBy;
 
-    public User(Integer userID, String userName, String password, Date createDate, String createdBy, Timestamp lastUpdate, String lastUpdatedBy) {
+
+
+    public User(Integer userID, String userName) {
         this.userID = userID;
         this.userName = userName;
-        this.password = password;
-        this.createDate = createDate;
-        this.createdBy = createdBy;
-        this.lastUpdate = lastUpdate;
-        this.lastUpdatedBy = lastUpdatedBy;
+
     }
 
     public Integer getUserID() {
@@ -39,44 +32,5 @@ public class User {
         this.userName = userName;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Timestamp getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Timestamp lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-
-    public String getLastUpdatedBy() {
-        return lastUpdatedBy;
-    }
-
-    public void setLastUpdatedBy(String lastUpdatedBy) {
-        this.lastUpdatedBy = lastUpdatedBy;
-    }
 }
 
