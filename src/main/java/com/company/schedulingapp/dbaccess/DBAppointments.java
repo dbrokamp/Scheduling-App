027b4be7a911ddefa0fbf89c49658754716993a6
Drew Brokamp <drew.brokamp@gmail.comm>
2022-06-27 09:04:59 -0400

    Added javadoc comments

--- src/main/java/com/company/schedulingapp/dbaccess/DBAppointments.java
+++ src/main/java/com/company/schedulingapp/dbaccess/DBAppointments.java
@@ -8,11 +8,19 @@ import java.sql.*;
 import java.time.LocalDateTime;
 
 
-
+/**
+ * Allows for access to appointments table in database
+ */
 public class DBAppointments {
 
     final private static ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
 
+    /**
+     * Gets appointments for specified user id
+     * @param userID user to get appointments for
+     * @return list of appointments by user
+     * @throws SQLException error with sql query
+     */
     public static ObservableList<Appointment> getUserAppointments(Integer userID) throws SQLException {
         ObservableList<Appointment> userAppointments = FXCollections.observableArrayList();
 
@@ -43,6 +51,12 @@ public class DBAppointments {
 
     }
 
+    /**
+     * Gets appointment list for specified customer
+     * @param customerID customer id to get appointments for
+     * @return list of customer's appointments
+     * @throws SQLException sql query error
+     */
     public static ObservableList<Appointment> getCustomerAppointments(Integer customerID) throws SQLException {
         ObservableList<Appointment> customerAppointments = FXCollections.observableArrayList();
 
@@ -71,6 +85,11 @@ public class DBAppointments {
         return customerAppointments;
     }
 
+    /**
+     * Gets all appointments from database
+     *
+     * @throws SQLException sql query error
+     */
     private static void getAllAppointmentsFromDatabase() throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "SELECT * FROM appointments";
@@ -93,6 +112,11 @@ public class DBAppointments {
         }
     }
 
+    /**
+     * Gets last appointmentID Integer and increases it by one
+     * @return Integer of new appointment id
+     * @throws SQLException error in sql query
+     */
     private static Integer createNewAppointmentID() throws SQLException {
         if (allAppointments.isEmpty()) {
             getAllAppointmentsFromDatabase();
@@ -103,6 +127,11 @@ public class DBAppointments {
         return currentLastAppointment.getAppointmentID() + 1;
     }
 
+    /**
+     * Getter for allAppointments
+     * @return list of all appointments
+     * @throws SQLException sql query error
+     */
     public static ObservableList<Appointment> getAllAppointments() throws SQLException {
         if (allAppointments.isEmpty()) {
             getAllAppointmentsFromDatabase();
@@ -114,6 +143,20 @@ public class DBAppointments {
         return allAppointments;
     }
 
+    /**
+     * Adds a new appointment to the database
+     *
+     * @param title for new appointment
+     * @param description for new appointment
+     * @param location for new appointment
+     * @param type for new appointment
+     * @param start for new appointment
+     * @param end for new appointment
+     * @param customerID for new appointment
+     * @param userID for new appointment
+     * @param contactName for new appointment
+     * @throws SQLException sql query error
+     */
     public static void addNewAppointment(String title, String description, String location, String type, Timestamp start, Timestamp end, Integer customerID, Integer userID, String contactName) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "INSERT INTO appointments VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
@@ -136,10 +179,20 @@ public class DBAppointments {
 
     }
 
+    /**
+     * Public access to delete from database action
+     * @param appointmentID appointment to delete
+     * @throws SQLException SQL query error
+     */
     public static void deleteAppointmentAction(Integer appointmentID) throws SQLException {
         deleteAppointmentFromDatabase(appointmentID);
     }
 
+    /**
+     * Deletes from database
+     * @param appointmentID appointment to delete
+     * @throws SQLException SQL query error
+     */
     private static void deleteAppointmentFromDatabase(Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "DELETE FROM appointments WHERE Appointment_ID = ?";
@@ -148,6 +201,11 @@ public class DBAppointments {
         deleteAppointmentStatement.executeUpdate();
     }
 
+    /**
+     * Update the last updated record for an appointment
+     * @param appointmentID appointment to update
+     * @throws SQLException SQL error
+     */
     private static void updateLastUpdateTimeField(Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET Last_Update = ? WHERE Appointment_ID = ?";
@@ -157,6 +215,11 @@ public class DBAppointments {
         updateAppointmentStatement.executeUpdate();
     }
 
+    /**
+     * Update the last updated by field for an appointment
+     * @param appointmentID appointment to update
+     * @throws SQLException SQL error
+     */
     private static void updateLastUpdatedByField(Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET Last_Updated_By = ? WHERE Appointment_ID = ?";
@@ -166,6 +229,12 @@ public class DBAppointments {
         updateAppointmentStatement.executeUpdate();
     }
 
+    /**
+     * Updates title field in database record
+     * @param newAppointmentTitle title to update
+     * @param appointmentID appointment to update
+     * @throws SQLException SQL error
+     */
     public static void updateAppointmentTitle(String newAppointmentTitle, Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET Title = ? WHERE Appointment_ID = ?";
@@ -177,6 +246,12 @@ public class DBAppointments {
         updateLastUpdatedByField(appointmentID);
     }
 
+    /**
+     * Updates description field in database record
+     * @param newAppointmentDescription description to update
+     * @param appointmentID - appointment to update
+     * @throws SQLException SQL error
+     */
     public static void updateAppointmentDescription(String newAppointmentDescription, Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET Description = ? WHERE Appointment_ID = ?";
@@ -188,6 +263,12 @@ public class DBAppointments {
         updateLastUpdatedByField(appointmentID);
     }
 
+    /**
+     * Updates record of appointment location
+     * @param newAppointmentLocation location to update
+     * @param appointmentID - appointment to update
+     * @throws SQLException SQL error
+     */
     public static void updateAppointmentLocation(String newAppointmentLocation, Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET Location = ? WHERE Appointment_ID = ?";
@@ -199,6 +280,12 @@ public class DBAppointments {
         updateLastUpdatedByField(appointmentID);
     }
 
+    /**
+     * Updates record of appointment type
+     * @param newAppointmentType type to update
+     * @param appointmentID appointment to update
+     * @throws SQLException SQL error
+     */
     public static void updateAppointmentType(String newAppointmentType, Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET Type = ? WHERE Appointment_ID = ?";
@@ -210,6 +297,12 @@ public class DBAppointments {
         updateLastUpdatedByField(appointmentID);
     }
 
+    /**
+     * Updates record of start time in database
+     * @param newAppointmentStart new start time
+     * @param appointmentID appointment to update
+     * @throws SQLException SQL error
+     */
     public static void updateAppointmentStart(Timestamp newAppointmentStart, Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET Start = ? WHERE Appointment_ID = ?";
@@ -221,6 +314,12 @@ public class DBAppointments {
         updateLastUpdatedByField(appointmentID);
     }
 
+    /**
+     * Updates record of end time in database
+     * @param newAppointmentEnd new end time
+     * @param appointmentID appointment to update
+     * @throws SQLException SQL Error
+     */
     public static void updateAppointmentEnd(Timestamp newAppointmentEnd, Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET End = ? WHERE Appointment_ID = ?";
@@ -232,6 +331,12 @@ public class DBAppointments {
         updateLastUpdatedByField(appointmentID);
     }
 
+    /**
+     * Updates customer id record in database
+     * @param newAppointmentCustomerID new customer id
+     * @param appointmentID appointment to update
+     * @throws SQLException SQL error
+     */
     public static void updateAppointmentCustomerID(Integer newAppointmentCustomerID, Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET Customer_ID = ? WHERE Appointment_ID = ?";
@@ -243,6 +348,12 @@ public class DBAppointments {
         updateLastUpdatedByField(appointmentID);
     }
 
+    /**
+     * Updates record of user id in database
+     * @param newAppointmentUserID new user id
+     * @param appointmentID appointment to update
+     * @throws SQLException SQL error
+     */
     public static void updateAppointmentUserID(Integer newAppointmentUserID, Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET Customer_ID = ? WHERE Appointment_ID = ?";
@@ -254,6 +365,12 @@ public class DBAppointments {
         updateLastUpdatedByField(appointmentID);
     }
 
+    /**
+     * Updates record of contact id in database
+     * @param newAppointmentContactID new contact id
+     * @param appointmentID appointment to update
+     * @throws SQLException SQL error
+     */
     public static void updateAppointmentContactID(Integer newAppointmentContactID, Integer appointmentID) throws SQLException {
         Connection connection = JDBC.getConnection();
         String sql = "UPDATE appointments SET Customer_ID = ? WHERE Appointment_ID = ?";
